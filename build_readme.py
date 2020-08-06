1d664d88c7a94cf1288e698b2912f9de1a3e06b5
Per Søderlind <per@soderlind.no>
2020-08-06 21:37:48 +0200

    fix syntax error

--- build_readme.py
+++ build_readme.py
@@ -116,8 +116,7 @@ def fetch_tweets():
         {
             "title": entry.text,
             # "url": entry.entities["urls"][0]["expanded_url"],
-            "url": ' '.join(url['expanded_url'] for url in
-                            entry.entities["urls"])
+            "url": ' '.join(url['expanded_url'] for url in entry.entities["urls"]),
             "published": entry.created_at.strftime('%d.%m.%Y'),
         }
         for entry in api.user_timeline()

0861a032f677f4babbd6e3220356302310b5af5d
Per Søderlind <per@soderlind.no>
2020-08-06 18:52:30 +0200

    add twitter feed

--- build_readme.py
+++ build_readme.py
@@ -1,5 +1,6 @@
 from python_graphql_client import GraphqlClient
 import feedparser
+import tweepy
 import time
 import httpx
 import json
@@ -12,6 +13,10 @@ client = GraphqlClient(endpoint="https://api.github.com/graphql")
 
 
 TOKEN = os.environ.get("SODERLIND_TOKEN", "")
+TWITTER_CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY", "")
+TWITTER_CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET", "")
+TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN", "")
+TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET", "")
 
 
 def replace_chunk(content, marker, chunk, inline=False):
@@ -102,7 +107,18 @@ def fetch_releases(oauth_token):
 #         "https://til.simonwillison.net/til.json",
 #         params={"sql": sql, "_shape": "array",},
 #     ).json()
-
+def fetch_tweets():
+	auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+	auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+	api = tweepy.API(auth)
+	return [
+		{
+            "title": entry["title"],
+            "url": entry["entities"]["expanded_url"].split("#")[0],
+            "published": time.strftime('%d.%m.%Y',entry["created_at"]),
+		}
+		for entry in api.user_timeline()
+	]
 
 def fetch_read():
     entries = feedparser.parse("https://getpocket.com/users/soderlind/feed/all")["entries"]
@@ -132,33 +148,33 @@ if __name__ == "__main__":
     project_releases = root / "releases.md"
     releases = fetch_releases(TOKEN)
     releases.sort(key=lambda r: r["published_at"], reverse=True)
-    md = "\n".join(
-        [
-            "* [{repo} {release}]({url}) - {published_day}".format(**release)
-            for release in releases[:8]
-        ]
-    )
-    readme_contents = readme.open().read()
-    rewritten = replace_chunk(readme_contents, "recent_releases", md)
+    # md = "\n".join(
+    #     [
+    #         "* [{repo} {release}]({url}) - {published_day}".format(**release)
+    #         for release in releases[:8]
+    #     ]
+    # )
+    # readme_contents = readme.open().read()
+    # rewritten = replace_chunk(readme_contents, "recent_releases", md)
 
-    # Write out full project-releases.md file
-    project_releases_md = "\n".join(
-        [
-            (
-                "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_day}\n"
-                "<br>{description}"
-            ).format(**release)
-            for release in releases
-        ]
-    )
-    project_releases_content = project_releases.open().read()
-    project_releases_content = replace_chunk(
-        project_releases_content, "recent_releases", project_releases_md
-    )
-    project_releases_content = replace_chunk(
-        project_releases_content, "release_count", str(len(releases)), inline=True
-    )
-    project_releases.open("w").write(project_releases_content)
+    # # Write out full project-releases.md file
+    # project_releases_md = "\n".join(
+    #     [
+    #         (
+    #             "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_day}\n"
+    #             "<br>{description}"
+    #         ).format(**release)
+    #         for release in releases
+    #     ]
+    # )
+    # project_releases_content = project_releases.open().read()
+    # project_releases_content = replace_chunk(
+    #     project_releases_content, "recent_releases", project_releases_md
+    # )
+    # project_releases_content = replace_chunk(
+    #     project_releases_content, "release_count", str(len(releases)), inline=True
+    # )
+    # project_releases.open("w").write(project_releases_content)
 
     # tils = fetch_tils()
     # tils_md = "\n".join(
@@ -172,6 +188,12 @@ if __name__ == "__main__":
     #     ]
     # )
     # rewritten = replace_chunk(rewritten, "tils", tils_md)
+
+    entries = fetch_tweets()[:5]
+    tweet_md = "\n".join(
+        ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "recent_releases", tweet_md)
 
     entries = fetch_read()[:5]
     read_md = "\n".join(

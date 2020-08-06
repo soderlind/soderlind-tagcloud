b621b87c0bbb8190af3a26568c9fed311b910a72
Per Søderlind <per@soderlind.no>
2020-08-06 19:28:25 +0200

    get tweet url

--- build_readme.py
+++ build_readme.py
@@ -114,10 +114,10 @@ def fetch_tweets():
 	return [
 		{
             "title": entry.text,
-            "url": entry.entities["urls"]["expanded_url"],
+            "url": entry.entities["urls"][0].expanded_url,
             "published": time.strftime('%d.%m.%Y',entry.created_at),
 		}
-		for entry in api.user_timeline(tweetmode="extended")
+		for entry in api.user_timeline()
 	]
 
 def fetch_read():

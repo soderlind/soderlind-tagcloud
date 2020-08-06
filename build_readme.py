0b3aaa6c07798a8281d0c361551c14a58ec93bcb
Per Søderlind <per@soderlind.no>
2020-08-06 23:10:08 +0200

    remove whitespace

--- build_readme.py
+++ build_readme.py
@@ -114,7 +114,7 @@ def fetch_tweets():
     api = tweepy.API(auth)
     return [
         {
-            "title": entry.text,
+            "title": entry.text.strip(),
             # "url": entry.entities["urls"][0]["expanded_url"],
             # "url": ' '.join(url['url'] for url in entry.entities["urls"])
            	"url": 'https://twitter.com/user/status/' + entry.id_str,

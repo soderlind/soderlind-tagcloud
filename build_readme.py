33d97bcf70a09bcfcee50220ff519f4dd1c9107f
Per Søderlind <per@soderlind.no>
2020-08-06 16:30:07 +0200

    dafte format

--- build_readme.py
+++ build_readme.py
@@ -109,7 +109,7 @@ def fetch_read():
         {
             "title": entry["title"],
             "url": entry["link"].split("#")[0],
-            "published": entry["published"].strftime("%d.%m.%Y"),
+            "published": entry["updated_parsed"].strftime("%d.%m.%Y"),
         }
         for entry in entries
     ]

b66a8a8c3ad30f6bb198dd874befd06d0cd4e65a
Per Søderlind <per@soderlind.no>
2020-08-06 21:55:09 +0200

    fix tweet url

--- build_readme.py
+++ build_readme.py
@@ -117,7 +117,7 @@ def fetch_tweets():
             "title": entry.text,
             # "url": entry.entities["urls"][0]["expanded_url"],
             # "url": ' '.join(url['url'] for url in entry.entities["urls"])
-         	"url": entry.source_url,
+           	"url": 'https://twitter.com/user/status/'.join(entry.id_str),
             "published": entry.created_at.strftime('%d.%m.%Y'),
         }
         for entry in api.user_timeline('soderlind')
